2ea892811ec7344ddc130c02b6f381353f4c413a
MYD <2663797538@qq.com>
2023-03-08 16:33:05 +0800

    fix navigate

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,11 +1,14 @@
+'use client';
 import {FC, useEffect} from "react";
 import {Button, Text} from "@fluentui/react-components";
 import {useRouter} from "next/router";
+import { useRouter as useNavi } from 'next/navigation';
 import FluidWrapper from "@/components/layouts/fluidWrapper";
 
 const Header: FC = () => {
   const brand = "MYD's blog";
   const router = useRouter();
+  const navi = useNavi();
   const routes = [
     {name: '首页', link: '/#blog-index', key: '/'},
     {name: '归档', link: '/archives/1', key: 'archives'},
@@ -26,7 +29,7 @@ const Header: FC = () => {
           {routes.map(o => <Button key={o.link}
                                    appearance={'subtle'}
                                    style={{color: matchRoute(o) ? 'var(--colorNeutralForeground1)' : '#696969'}}
-                                   onClick={() => router.replace(o.link)}>{o.name}</Button>)}
+                                   onClick={() => navi.replace(o.link)}>{o.name}</Button>)}
         </div>
       </header>
     </FluidWrapper>
