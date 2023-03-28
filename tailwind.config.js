946ea2784ca12b29e8b20179b4d4da5a0af437bf
MYD <2663797538@qq.com>
2023-03-28 16:38:30 +0800

    fix Safari style

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,29 +7,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      height: {
-        // layout: 'calc(100vh - 44px)',
-        layout: [
-          '-webkit-fill-available',
-          {
-            '@supports (-webkit-touch-callout: none)': {
-              height: '-webkit-fill-available',
-            },
-          },
-          'calc(100vh - 44px)',
-        ],
-      },
-      minHeight: {
-        layout: [
-          '-webkit-fill-available',
-          {
-            '@supports (-webkit-touch-callout: none)': {
-              height: '-webkit-fill-available',
-            },
-          },
-          'calc(100vh - 44px)',
-        ]
-      },
+
     },
   },
   plugins: [],
