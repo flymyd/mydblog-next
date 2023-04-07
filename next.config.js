3f21f39415c92f6858637ad95e3b0cecc42dad11
MYD <2663797538@qq.com>
2023-04-07 18:10:13 +0800

    deploy trigger

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
     remarkPlugins: [],
     rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
   },
 })
@@ -14,7 +10,6 @@ const withMDX = require('@next/mdx')({
 const path = require('path')
 const nextConfig = {
   swcMinify: true,
-  // Configure pageExtensions to include md and mdx
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: false,
   sassOptions: {
