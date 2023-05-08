9fda68981ce2601bdeaa825c4155ce1d0ef06e18
MYD <2663797538@qq.com>
2023-05-08 15:35:31 +0800

    update config

--- pages/api/getPoeConfig.ts
+++ pages/api/getPoeConfig.ts
@@ -6,6 +6,6 @@ export default function handler(
   res: NextApiResponse
 ) {
   res.status(200).json({
-    link: "https://sub.xeton.dev/sub?target=clash&new_name=true&url=https%3A%2F%2Fsub1.smallstrawberry.com%2Fapi%2Fv1%2Fclient%2Fsubscribe%3Ftoken%3Drnci40v8yonukj98n86wcmfp6v5pm30v&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini"
+    link: "https://sub.xeton.dev/sub?target=clash&new_name=true&url=https%3A%2F%2Fsub1.smallstrawberry.com%2Fapi%2Fv1%2Fclient%2Fsubscribe%3Ftoken%3Dxjr9re4d9frnnibqdwsxkv1yd6wuirla&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini"
   })
 }
